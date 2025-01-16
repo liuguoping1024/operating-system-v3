86f2042b78ed34732b6f6551701554295bb796d9
liuguoping1024 <liuguoping@gmail.com>
2025-01-15 21:57:41 -0500

    add new devices

---
+++ buildroot-external/board/thirdreality/old/jethub-j100/image-hook.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# shellcheck disable=SC2155
+
+function hassos_pre_image() {
+    local BOOT_DATA="$(path_boot_dir)"
+    local UBOOT="${BINARIES_DIR}/u-boot.bin.sd.bin"
+    local SPL_IMG="$(path_spl_img)"
+
+    cp "${BINARIES_DIR}/boot.scr" "${BOOT_DATA}/boot.scr"
+    mkdir -p "${BOOT_DATA}/amlogic"
+    cp "${BINARIES_DIR}/meson-axg-jethome-jethub-j100.dtb" "${BOOT_DATA}/amlogic/"
+    cp "${BINARIES_DIR}/meson-axg-jethome-jethub-j110-rev-2.dtb" "${BOOT_DATA}/amlogic/"
+    cp "${BINARIES_DIR}/meson-axg-jethome-jethub-j110-rev-3.dtb" "${BOOT_DATA}/amlogic/"
+
+    cp "${BOARD_DIR}/boot-env.txt" "${BOOT_DATA}/hassos-config.txt" || true
+    cp "${BOARD_DIR}/cmdline.txt" "${BOOT_DATA}/cmdline.txt"
+
+    # SPL
+    create_spl_image
+    dd if="${UBOOT}" of="${SPL_IMG}" bs=1 count=442 conv=fsync
+    dd if="${UBOOT}" of="${SPL_IMG}" bs=512 skip=1 seek=1 conv=fsync
+}
+
+function hassos_post_image() {
+    convert_disk_image_xz
+    # support for create AmLogic burnable images
+    [[ -f "${BINARIES_DIR}/platform.conf" ]] && convert_disk_image_burn_zip
+}
